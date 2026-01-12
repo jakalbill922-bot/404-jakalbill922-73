e465e1bc60f01662bd15ff86c732c546940b301f
ZeusAIGod <zeusai909@gmail.com>
2026-01-12 09:15:15 +0100

    rm

--- test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "http://localhost:10006/generate"
-
-response = requests.post(url, files={"prompt_image_file": open("cr7.png", "rb")})
-
-# save the response content to ply file
-with open("response.ply", "wb") as f:
-    f.write(response.content)
